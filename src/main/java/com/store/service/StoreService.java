7ecba6d331b5b3321b7876506a2c89f017e2f282
Hosein Mostajabi <Hosein.mostajabii@gmail.com>
2025-11-27 14:11:20 +0330

    Added addInventory

--- src/main/java/com/store/service/StoreService.java
+++ src/main/java/com/store/service/StoreService.java
@@ -2,9 +2,12 @@ package com.store.service;
 
 import com.store.dao.InvoiceDAO;
 import com.store.dao.ProductDAO;
+import com.store.db.DBConnection;
 import com.store.model.Product;
 
+import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.Optional;
 
 /**
  * منطق برنامه
@@ -27,5 +30,20 @@ public class StoreService {
     public Product addProduct(String sku, String name, double price, int qty) throws SQLException {
         Product product = new Product(null, sku, name, price, qty);
         return productDAO.insert(product);
+    }
+
+    /**
+     * افزایش موجودی (خرید برای انبار)- به صورت اتوماتیک مقدار جدید را در جدول ذخیره میکند
+     */
+    public void addInventory(String sku, int addQty) throws SQLException {
+        try (Connection connection = DBConnection.getConnection()) {
+            Optional<Product> optional = productDAO.findBySku(sku);
+            if (optional.isPresent()) {
+                throw new SQLException("Product not found");
+            }
+            Product p = optional.get();
+            int newQty = p.quantity + addQty;
+            productDAO.updateQuantity(p.id, newQty, connection);
+        }
     }
 }
