be59d26307c09ec47e649e0069c0acb632cfde77
Hosein Mostajabi <Hosein.mostajabii@gmail.com>
2025-11-28 19:47:51 +0330

    Added showMenu to ConsoleUI

--- src/main/java/com/store/ui/ConsoleUI.java
+++ src/main/java/com/store/ui/ConsoleUI.java
@@ -9,6 +9,16 @@ import java.util.Scanner;
  */
 public class ConsoleUI {
 
+    private void showMenu() {
+        print("\n === سیستم مدیریت فروشگاه مواد غذایی ===");
+        print("1. ثبت کالای جدید");
+        print("2. خرید (افزایش موجودی)");
+        print("3. فروش / صدور فاکتور");
+        print("4. نمایش همه کالا ها");
+        print("5. مشاهده موجودی انبار (کالاها با تعداد)");
+        print("0. خروج");
+    }
+
     private void print(String message) {
         System.out.println(message);
     }
